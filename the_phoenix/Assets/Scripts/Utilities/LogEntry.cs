9a76dd347005aa9f1171e472a7e9ae096043653d
jstarace <65109793+jstarace@users.noreply.github.com>
2023-03-18 11:55:46 -0700

    Starting Logging
    Creating the base files, more to come

---
+++ the_phoenix/Assets/Scripts/Utilities/LogEntry.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System;
+
+[System.Serializable]
+
+public class LogEntry
+{
+    [SerializeField] public string date;
+    [SerializeField] public string time;
+    [SerializeField] public string userId;
+    [SerializeField] public string userName;
+    [SerializeField] public string userType;
+    
+
+
+}
